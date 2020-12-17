374998a5c5df41b4b697c33fbf997d2edf540b29
pantelis <pantelis@panteloPc>
2020-12-17 20:22:55 +0200

    Added

--- intranet/src/intranet/hua/controller/SupervisorController.java
+++ intranet/src/intranet/hua/controller/SupervisorController.java
@@ -1,13 +1,12 @@
 package intranet.hua.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SupervisorController {
 	
-	@GetMapping("/supervisor")
+	@RequestMapping(value={"/","/home","/supervisor"})
 	public String hello() {
 		return "supervisor";
 	}
