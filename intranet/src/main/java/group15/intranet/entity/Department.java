a40623c36bcc1e248db71142abc936d49a03fe42
xdmar <xdmar@192.168.56.1>
2020-12-24 18:32:13 +0200

    added the rest of the entity classes

---
+++ intranet/src/main/java/group15/intranet/entity/Department.java
@@ -0,0 +1,75 @@
+package group15.intranet.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "dept_id")
+	private int deptID;
+
+	@OneToMany(mappedBy = "dept")
+	private List<User> users;
+
+	@Column(name = "dept_name")
+	private String deptName;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "super_id")
+	private User supervisor;
+
+	public int getDeptID() {
+		return deptID;
+	}
+
+	public void setDeptID(int deptID) {
+		this.deptID = deptID;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+
+	public User getSupervisor() {
+		return supervisor;
+	}
+
+	public void setSupervisor(User supervisor) {
+		this.supervisor = supervisor;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [deptID=" + deptID + ", users=" + users + ", deptName=" + deptName + ", supervisor="
+				+ supervisor + "]";
+	}
+
+	public Department() {
+	}
+}
---
+++ intranet/src/main/java/group15/intranet/entity/Role.java
@@ -0,0 +1,54 @@
+package group15.intranet.entity;
+
+import java.util.List;
+import javax.persistence.JoinColumn;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "role_name")
+	private String roleName;
+
+	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH,
+			CascadeType.REFRESH })
+	@JoinTable(name = "rolesbyuser", joinColumns = @JoinColumn(name = "role_name"), inverseJoinColumns = @JoinColumn(name = "user_id"))
+	private List<User> users;
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [roleName=" + roleName + ", users=" + users + "]";
+	}
+
+	public Role() {
+
+	}
+
+}
