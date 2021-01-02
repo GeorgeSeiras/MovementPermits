82478a57ed6b8372b30ddd99889511702ccc780b
pantel70 <pantelkommat@gmail.com>
2021-01-02 22:29:16 +0200

    updated

--- intranet/src/main/java/group15/intranet/controller/AdminController.java
+++ intranet/src/main/java/group15/intranet/controller/AdminController.java
@@ -13,55 +13,34 @@ public class AdminController {
 		return "admin";
 	}
 	
-	@GetMapping("addUser")
+	@GetMapping("user")
 	public String adminAddUser() {
-		return "add-user";
+		return "admin-users";
 	}
 
-	@GetMapping("deleteUser")
+	@GetMapping("user/create")
 	public String adminDeleteUser() {
-		return "update-user";
+		return "admin-user-create";
 	}
 
-	@GetMapping("updateUser")
-	public String adminUpdateUser() {
-		return "add-user";
+	@GetMapping("department")
+	public String adminCreateDepartment() {
+		return "admin-departments";
 	}
 	
-	@GetMapping("createDepartment")
-	public String adminCreateDepartment() {
+	@GetMapping("department/create")
+	public String adminDeleteDepartment() {
 		return "admin-department-create";
 	}
 	
-	@GetMapping("deleteDepartment")
-	public String adminDeleteDepartment() {
-		return "admin-department-delete";
-	}
-	
-	@GetMapping("createRole")
+	@GetMapping("role")
 	public String adminCreateRole() {
-		return "create-role";
+		return "admin-roles";
 	}
 	
-	@GetMapping("deleteRole")
+	@GetMapping("role/create")
 	public String adminDeleteRole() {
-		return "delete-role";
+		return "admin-roles-create";
 	}
-	
-	@GetMapping("assignRole")
-	public String assignRole() {
-		return "assign-role";
-	}
-	
-	@GetMapping("changeSupervisor")
-	public String adminChangeSupervisor() {
-		return "create-role";
-	}
-	
-	
-	
-	
-	
-	
 	
 }
