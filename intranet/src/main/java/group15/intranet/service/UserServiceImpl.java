df9f3a774ac694f322f51c124752ecf448e5906b
GeorgeSeiras <gsiras1999@gmail.com>
2021-01-03 21:41:51 +0200

    small change on service

--- intranet/src/main/java/group15/intranet/service/UserServiceImpl.java
+++ intranet/src/main/java/group15/intranet/service/UserServiceImpl.java
@@ -14,17 +14,14 @@ import group15.intranet.repository.DepartmentRepository;
 import group15.intranet.repository.UserRepository;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@Autowired
 	DepartmentRepository depRepository;
-	
-	
-	
-	
+
 	@Override
 	public List<User> getAllUsers() {
 		return userRepository.findAll();
@@ -33,64 +30,55 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public ResponseEntity<User> addUser(User user) {
 		User checkedUser = userRepository.findByUserID(user.getUserID());
-		if(checkedUser!=null) {
-			return new ResponseEntity<User>(user,HttpStatus.ALREADY_REPORTED);
+		if (checkedUser != null) {
+			return new ResponseEntity<User>(user, HttpStatus.ALREADY_REPORTED);
 		}
 		userRepository.save(user);
-		return new ResponseEntity<User>(user,HttpStatus.OK); 
+		return new ResponseEntity<User>(user, HttpStatus.OK);
 	}
-	
+
 	@Override
 	public ResponseEntity<User> updateUser(User user) {
 		User checkedUser = userRepository.findByUserID(user.getUserID());
 		System.out.println(checkedUser);
 
-		if(checkedUser==null) {
-			return new ResponseEntity<User>(user,HttpStatus.NOT_FOUND);
+		if (checkedUser == null) {
+			return new ResponseEntity<User>(user, HttpStatus.NOT_FOUND);
 		}
-		if(user.getFname()!=null) {
-			checkedUser.setFname(user.getFname());
-		}
-		if(user.getLname()!=null) {
-			checkedUser.setLname(user.getLname());
-		}
-		if(user.getPhoneNum()!=null) {
-			checkedUser.setPhoneNum(user.getPhoneNum());
-		}
-		if(user.getAddress()!=null) {
-			checkedUser.setAddress(user.getAddress());
-		}
-		//checkedUser.setDept(depRepository.findById(user.getDept().getDeptID()));
+		checkedUser.setFname(user.getFname());
+		checkedUser.setLname(user.getLname());
+		checkedUser.setPhoneNum(user.getPhoneNum());
+		checkedUser.setAddress(user.getAddress());
+		// checkedUser.setDept(depRepository.findById(user.getDept().getDeptID()));
+		System.out.println(checkedUser);
 		userRepository.save(checkedUser);
-		return new ResponseEntity<User>(checkedUser,HttpStatus.OK);
+		return new ResponseEntity<User>(checkedUser, HttpStatus.OK);
 	}
-
-
 
 	@Override
 	public ResponseEntity<User> deleteUser(int id) {
 		User checkedUser = userRepository.findByUserID(id);
-		if(checkedUser==null) {
-			return new ResponseEntity<User>(checkedUser,HttpStatus.NOT_FOUND);
+		if (checkedUser == null) {
+			return new ResponseEntity<User>(checkedUser, HttpStatus.NOT_FOUND);
 		}
 		userRepository.delete(checkedUser);
-		return new ResponseEntity<User>(checkedUser,HttpStatus.OK);
+		return new ResponseEntity<User>(checkedUser, HttpStatus.OK);
 	}
-	
+
 	@Override
 	public ResponseEntity<User> findUserById(int id) {
 		User checkedUser = userRepository.findByUserID(id);
-		if(checkedUser == null) {
-			return new ResponseEntity<User>(checkedUser,HttpStatus.NOT_FOUND);
+		if (checkedUser == null) {
+			return new ResponseEntity<User>(checkedUser, HttpStatus.NOT_FOUND);
 		}
-		return new ResponseEntity<User>(checkedUser,HttpStatus.OK);
+		return new ResponseEntity<User>(checkedUser, HttpStatus.OK);
 	}
 
 	@Override
 	public ResponseEntity<User> assignRoleToUser(Role role, User user) {
 		User temp = userRepository.findByUserID(user.getUserID());
-		if(temp == null) {
-			return new ResponseEntity<User>(user,HttpStatus.NOT_FOUND);
+		if (temp == null) {
+			return new ResponseEntity<User>(user, HttpStatus.NOT_FOUND);
 		}
 		List<Role> roles = new ArrayList();
 		roles = userRepository.findByUserID(user.getUserID()).getAuthorities();
