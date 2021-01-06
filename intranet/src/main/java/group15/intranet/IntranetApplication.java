2dc455a75b4884af381cae0ee18d47a61fad8de8
GeorgeSeiras <gsiras1999@gmail.com>
2021-01-06 20:27:31 +0200

    swagger

--- intranet/src/main/java/group15/intranet/IntranetApplication.java
+++ intranet/src/main/java/group15/intranet/IntranetApplication.java
@@ -1,16 +1,27 @@
 package group15.intranet;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class IntranetApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(IntranetApplication.class, args);
 	}
+	@Bean
+    public Docket productApi() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("group15.intranet.rest_controller"))              
+          .build();                                           
+    }
 
 }
