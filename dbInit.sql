7447a1d7838f9d0a4a7e5ead4bdc3d8763db7a3a
GeorgeSeiras <46285035+GeorgeSeiras@users.noreply.github.com>
2021-01-07 18:10:46 +0200

    Delete dbInit.sql

--- dbInit.sql
+++
@@ -1,5 +0,0 @@
-use `ds`;
-INSERT INTO departments VALUES (1,'ADMIN',NULL);
-INSERT INTO users VALUES(0,'Adam','Smith','address','6980808080',1,"admin","$2a$10$qU.zkvG17NZ6f/m3N22wG.NWpuJwJh/zqqeAbnpBbj9uCA2G.wOni",1);
-INSERT INTO roles VALUES("ROLE_ADMIN");
-INSERT INTO authorities VALUES("ROLE_ADMIN","admin");
