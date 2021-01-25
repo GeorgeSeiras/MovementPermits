7e0fa0219710c93abd875db3f9238022875fe768
GeorgeSeiras <gsiras1999@gmail.com>
2021-01-25 20:19:05 +0200

    added ROLE_USER to dbInit.sql authorities

--- internet/src/components/createPermit.js
+++ internet/src/components/createPermit.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./createPermit.css";
+import Cookies from 'universal-cookie';
 class CreatePermit extends React.Component {
     constructor() {
         super();
@@ -9,54 +10,64 @@ class CreatePermit extends React.Component {
     };
 
     async componentDidMount() {
-        // const response = await fetch("http://localhost:8080/user/me", {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({ token: localStorage.getItem('token') })
-        // });
-        // if (!response.ok) {
-        //     document.getElementById("message").innerHTML = "There was an error while retrieving your data";
-        // }
-        // const data = await response.json();
-        // this.setState({ user: data });
-
+        const cookies = new Cookies();
+        const response = await fetch("http://localhost:8080/auth/me?jwt=" + cookies.get('token'));
+        if (!response.ok) {
+            document.getElementById("message").innerHTML = "There was an error while retrieving your data";
+        }
+        const data = await response.json();
+        this.setState({ user: data });
     }
+
+    calcEndDate() {
+        const type = document.getElementById("type").value;
+        if (document.getElementById("startDate").value !== "") {
+            document.getElementById("endDate").value = document.getElementById("startDate").value;
+            switch (type) {
+                case "daily":
+                    document.getElementById("endDate").stepUp();
+                    break;
+                case "weekly":
+                    document.getElementById("endDate").stepUp(7);
+                    break;
+                case "monthly":
+                    document.getElementById("endDate").stepUp(30);
+                    break;
+                default:
+            }
+        }
+    }
+
+    calcStartDate() {
+        const type = document.getElementById("type").value;
+        document.getElementById("startDate").value = document.getElementById("endDate").value;
+        switch (type) {
+            case "daily":
+                document.getElementById("startDate").stepDown();
+                break;
+            case "weekly":
+                document.getElementById("startDate").stepDown(7);
+                break;
+            case "monthly":
+                document.getElementById("startDate").stepDown(30);
+                break;
+            default:
+        }
+    }
+
 
     render() {
         return (
             <div className="container">
                 <h1>Fill the form to submit a movement permit application</h1>
                 <p> Address: <input type="text" id="address"></input></p>
-                <p>Type :<select id="type">
+                <p>Type :<select id="type" onChange={() => { this.calcEndDate() }}>
                     <option value="daily">Daily</option>
                     <option value="weekly">Weekly</option>
                     <option value="monthly">Montly</option>
                 </select></p>
-                <p> Start Date:<input type="date" id="startDate" onChange={() => {
-                    const type = document.getElementById("type").value;
-                    document.getElementById("endDate").value = document.getElementById("startDate").value;
-                    if (type === "daily") {
-                        document.getElementById("endDate").stepUp();
-
-                    } else if (type === "weekly") {
-                        document.getElementById("endDate").stepUp(7);
-                    } else if (type === "monthly") {
-                        document.getElementById("endDate").stepUp(30);
-                    }
-                }}></input></p>
-                <p> End Date:<input type="date" id="endDate" onChange={() => {
-                    const type = document.getElementById("type").value;
-                    document.getElementById("startDate").value = document.getElementById("endDate").value;
-                    if (type === "daily") {
-                        document.getElementById("startDate").stepDown();
-                    } else if (type === "weekly") {
-                        document.getElementById("startDate").stepDown(7);
-                    } else if (type === "monthly") {
-                        document.getElementById("startDate").stepDown(30);
-                    }
-                }}></input></p>
+                <p> Start Date:<input type="date" id="startDate" onChange={() => { this.calcEndDate() }}></input></p>
+                <p> End Date:<input type="date" id="endDate" onChange={() => { this.calcStartDate() }}></input></p>
                 <p id="message" className="message"></p>
                 <p>
                     <button onClick={async () => {
@@ -65,17 +76,23 @@ class CreatePermit extends React.Component {
                                 method: 'POST',
                                 headers: { 'Content-Type': 'application/json' },
                                 body: JSON.stringify({
-                                    address: document.getElementById("address").value,
-                                    type: document.getElementById("type").value,
-                                    startDate: document.getElementById("startDate").value,
-                                    endDate: document.getElementById("endDate").value,
-                                    status:"PENDING",
-                                    user:this.state.user.userID
+                                    permit: {
+                                        address: document.getElementById("address").value,
+                                        type: document.getElementById("type").value,
+                                        startDate: document.getElementById("startDate").value,
+                                        endDate: document.getElementById("endDate").value,
+                                        status: "PENDING"
+                                    },
+                                    userID: this.state.user.userID
                                 })
                             }
                             const response = await fetch("http://localhost:8080/permits", requestOptions)
-                            if (!response.ok) {
+                            if (response.ok) {
+                                document.getElementById("message").innerHTML = "Successfully created permit";
+                                document.getElementById("message").style.color = "green";
+                            } else if (response.status === 404) {
                                 document.getElementById("message").innerHTML = "There was an error while creating the permit";
+                                document.getElementById("message").style.color = "red";
                             }
                         }
                     }}>Submit</button>
